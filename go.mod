3da9d41701b4fd870326c1f8ffb06d62d4bfa7f5
Streamer272 <daniel.svitan.team7274dev@gmail.com>
2022-02-14 11:37:43 +0100

    :construction: Starts working on v2.0

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/sys v0.0.0-20220110181412-a018aaa089fe // indirect
--- main.go
+++ main.go
@@ -4,14 +4,9 @@ import (
 	"fmt"
 	"os"
 	"runtime"
-	"strconv"
 
-	"github.com/Streamer272/ipctl/config"
 	"github.com/Streamer272/ipctl/constants"
-	"github.com/Streamer272/ipctl/handle_error"
-	"github.com/Streamer272/ipctl/listener"
-	"github.com/Streamer272/ipctl/systemctl"
-	"github.com/akamensky/argparse"
+	cli "github.com/jawher/mow.cli"
 )
 
 func main() {
@@ -20,127 +15,146 @@ func main() {
 		os.Exit(1)
 	}
 
-	parser := argparse.NewParser("ipctl", "IP controller\nListen to IP change and change your DNS' records dynamically")
+	app := cli.App("ipctl", "IP controller - Listen to IP change and change your DNS' records dynamically")
+	app.Spec = ""
+	app.Version("v version", fmt.Sprintf("ipctl version %v", constants.VERSION))
 
-	helpCommand := parser.NewCommand("help", "Display help message")
-	versionCommand := parser.NewCommand("version", "Display program version")
-	versionFlag := parser.Flag("v", "version", &argparse.Options{Required: false, Help: "Display program version", Default: false})
+	app.Command("version", "Show the version and exit", func(cmd *cli.Cmd) {
+		cmd.Action = func() {
+			app.PrintVersion()
+		}
+	})
+	app.Command("help", "Print help and exit", func(cmd *cli.Cmd) {
+		cmd.Action = func() {
+			app.PrintLongHelp()
+		}
+	})
 
-	ipCommand := parser.NewCommand("ip", "Get current IP")
+	app.Command("ip", "Show current IP", func(cmd *cli.Cmd) {
+		cmd.Action = func() {
+			fmt.Printf("%v", listener)
+		}
+	})
 
-	configCommand := parser.NewCommand("config", "Manage configuration")
-	getCommand := configCommand.NewCommand("get", "Show current configuration")
-	field := getCommand.String("f", "field", &argparse.Options{Required: false, Help: "Field to get", Default: ""})
-	setCommand := configCommand.NewCommand("set", "Update configuration")
-	name := setCommand.String("n", "name", &argparse.Options{Required: true, Help: "Name of field to update"})
-	value := setCommand.String("V", "value", &argparse.Options{Required: true, Help: "New value"})
-	initConfigCommand := configCommand.NewCommand("init", "Initialize configuration")
-	rewriteCommand := configCommand.NewCommand("rewrite", "Rewrite configuration to default")
-	removeConfigCommand := configCommand.NewCommand("remove", "Remove configuration")
+	app.Run(os.Args)
 
-	serviceCommand := parser.NewCommand("service", "Manage service")
-	initServiceCommand := serviceCommand.NewCommand("init", "Initialize service")
-	enableFlag := initServiceCommand.Flag("e", "enable", &argparse.Options{Required: false, Help: "Enable systemctl service", Default: false})
-	removeServiceCommand := serviceCommand.NewCommand("remove", "Remove service")
-	enableCommand := serviceCommand.NewCommand("enable", "Enable listening service")
-	disableCommand := serviceCommand.NewCommand("disable", "Disable listening service")
-	statusCommand := serviceCommand.NewCommand("status", "Status of listening service")
-	startCommand := serviceCommand.NewCommand("start", "Start listening service")
-	stopCommand := serviceCommand.NewCommand("stop", "Stop listening service")
-	restartCommand := serviceCommand.NewCommand("restart", "Restart listening service")
-	reloadCommand := serviceCommand.NewCommand("reload", "Reload listening service")
-	logsCommand := serviceCommand.NewCommand("logs", "Show service logs")
-
-	listenCommand := parser.NewCommand("listen", "Listen to IP change")
-	updateCommand := parser.NewCommand("update", "Update DNS IP address")
-
-	err := parser.Parse(os.Args)
-	if err != nil || helpCommand.Happened() {
-		fmt.Print(parser.Usage(err))
-		os.Exit(0)
-	}
-	if *versionFlag || versionCommand.Happened() {
-		fmt.Printf("%v version %v\n", parser.GetName(), constants.VERSION)
-		os.Exit(0)
-	}
-
-	if ipCommand.Happened() {
-		ip, err := listener.GetCurrentIp()
-		if err != nil {
-			fmt.Printf("Couldn't get IP, error: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("%v", ip)
-		os.Exit(0)
-	}
-
-	if os.Getuid() != 0 {
-		fmt.Printf("You need to run %v as root\n", parser.GetName())
-		os.Exit(1)
-	}
-
-	if configCommand.Happened() {
-		if getCommand.Happened() {
-			if *field == "" {
-				fmt.Printf("%v\n", config.GetAll())
-			} else {
-				fmt.Printf("%v\n", config.Get(*field))
-			}
-		}
-		if setCommand.Happened() {
-			config.Set(*name, *value)
-		}
-		if initConfigCommand.Happened() {
-			config.Init(false)
-		}
-		if rewriteCommand.Happened() {
-			config.Init(true)
-		}
-		if removeConfigCommand.Happened() {
-			config.Remove()
-		}
-	}
-
-	if serviceCommand.Happened() {
-		if initServiceCommand.Happened() {
-			systemctl.Init(*enableFlag)
-		}
-		if removeServiceCommand.Happened() {
-			systemctl.Remove()
-		}
-		if enableCommand.Happened() {
-			systemctl.Enable()
-		}
-		if disableCommand.Happened() {
-			systemctl.Disable()
-		}
-		if statusCommand.Happened() {
-			systemctl.Status()
-		}
-		if startCommand.Happened() {
-			systemctl.Start()
-		}
-		if stopCommand.Happened() {
-			systemctl.Stop()
-		}
-		if restartCommand.Happened() {
-			systemctl.Restart()
-		}
-		if reloadCommand.Happened() {
-			systemctl.Reload()
-		}
-		if logsCommand.Happened() {
-			systemctl.Logs()
-		}
-	}
-
-	if listenCommand.Happened() {
-		interval, err := strconv.Atoi(config.Get("interval"))
-		handle_error.HandleError(err)
-
-		listener.Listen(config.Get("command"), interval)
-	}
-	if updateCommand.Happened() {
-		listener.Update(config.Get("command"))
-	}
+	//parser := argparse.NewParser("ipctl", "IP controller\nListen to IP change and change your DNS' records dynamically")
+	//
+	//helpCommand := parser.NewCommand("help", "Display help message")
+	//versionCommand := parser.NewCommand("version", "Display program version")
+	//versionFlag := parser.Flag("v", "version", &argparse.Options{Required: false, Help: "Display program version", Default: false})
+	//
+	//ipCommand := parser.NewCommand("ip", "Get current IP")
+	//
+	//configCommand := parser.NewCommand("config", "Manage configuration")
+	//getCommand := configCommand.NewCommand("get", "Show current configuration")
+	//field := getCommand.String("f", "field", &argparse.Options{Required: false, Help: "Field to get", Default: ""})
+	//setCommand := configCommand.NewCommand("set", "Update configuration")
+	//name := setCommand.String("n", "name", &argparse.Options{Required: true, Help: "Name of field to update"})
+	//value := setCommand.String("V", "value", &argparse.Options{Required: true, Help: "New value"})
+	//initConfigCommand := configCommand.NewCommand("init", "Initialize configuration")
+	//rewriteCommand := configCommand.NewCommand("rewrite", "Rewrite configuration to default")
+	//removeConfigCommand := configCommand.NewCommand("remove", "Remove configuration")
+	//
+	//serviceCommand := parser.NewCommand("service", "Manage service")
+	//initServiceCommand := serviceCommand.NewCommand("init", "Initialize service")
+	//enableFlag := initServiceCommand.Flag("e", "enable", &argparse.Options{Required: false, Help: "Enable systemctl service", Default: false})
+	//removeServiceCommand := serviceCommand.NewCommand("remove", "Remove service")
+	//enableCommand := serviceCommand.NewCommand("enable", "Enable listening service")
+	//disableCommand := serviceCommand.NewCommand("disable", "Disable listening service")
+	//statusCommand := serviceCommand.NewCommand("status", "Status of listening service")
+	//startCommand := serviceCommand.NewCommand("start", "Start listening service")
+	//stopCommand := serviceCommand.NewCommand("stop", "Stop listening service")
+	//restartCommand := serviceCommand.NewCommand("restart", "Restart listening service")
+	//reloadCommand := serviceCommand.NewCommand("reload", "Reload listening service")
+	//logsCommand := serviceCommand.NewCommand("logs", "Show service logs")
+	//
+	//listenCommand := parser.NewCommand("listen", "Listen to IP change")
+	//
+	//err := parser.Parse(os.Args)
+	//if err != nil || helpCommand.Happened() {
+	//	fmt.Print(parser.Usage(err))
+	//	os.Exit(0)
+	//}
+	//if *versionFlag || versionCommand.Happened() {
+	//	fmt.Printf("%v version %v\n", parser.GetName(), constants.VERSION)
+	//	os.Exit(0)
+	//}
+	//
+	//if ipCommand.Happened() {
+	//	ip, err := listener.GetCurrentIp()
+	//	if err != nil {
+	//		fmt.Printf("Couldn't get IP, error: %v\n", err)
+	//		os.Exit(1)
+	//	}
+	//	fmt.Printf("%v", ip)
+	//	os.Exit(0)
+	//}
+	//
+	//if os.Getuid() != 0 {
+	//	fmt.Printf("You need to run %v as root\n", parser.GetName())
+	//	os.Exit(1)
+	//}
+	//
+	//if configCommand.Happened() {
+	//	if getCommand.Happened() {
+	//		if *field == "" {
+	//			fmt.Printf("%v\n", config.GetAll())
+	//		} else {
+	//			fmt.Printf("%v\n", config.Get(*field))
+	//		}
+	//	}
+	//	if setCommand.Happened() {
+	//		config.Set(*name, *value)
+	//	}
+	//	if initConfigCommand.Happened() {
+	//		config.Init(false)
+	//	}
+	//	if rewriteCommand.Happened() {
+	//		config.Init(true)
+	//	}
+	//	if removeConfigCommand.Happened() {
+	//		config.Remove()
+	//	}
+	//}
+	//
+	//if listenCommand.Happened() {
+	//	interval, err := strconv.Atoi(config.Get("interval"))
+	//	handle_error.HandleError(err)
+	//
+	//	listener.Listen(config.Get("command"), interval)
+	//}
+	//
+	//if serviceCommand.Happened() {
+	//	if initServiceCommand.Happened() {
+	//		systemctl.Init(*enableFlag)
+	//	}
+	//	if removeServiceCommand.Happened() {
+	//		systemctl.Remove()
+	//	}
+	//	if enableCommand.Happened() {
+	//		systemctl.Enable()
+	//	}
+	//	if disableCommand.Happened() {
+	//		systemctl.Disable()
+	//	}
+	//	if statusCommand.Happened() {
+	//		systemctl.Status()
+	//	}
+	//	if startCommand.Happened() {
+	//		systemctl.Start()
+	//	}
+	//	if stopCommand.Happened() {
+	//		systemctl.Stop()
+	//	}
+	//	if restartCommand.Happened() {
+	//		systemctl.Restart()
+	//	}
+	//	if reloadCommand.Happened() {
+	//		systemctl.Reload()
+	//	}
+	//	if logsCommand.Happened() {
+	//		systemctl.Logs()
+	//	}
+	//}
 }
