4b41641c147051838743413eae0544c075391120
Streamer272 <daniel.svitan.team7274dev@gmail.com>
2022-02-14 12:08:48 +0100

    :hammer: Refactors config

--- config/config.go
+++ config/config.go
@@ -1,68 +1,32 @@
 package config
 
 import (
-	"encoding/json"
-	"errors"
-	"github.com/Streamer272/ipctl/constants"
-	"github.com/Streamer272/ipctl/handle_error"
-	"github.com/Streamer272/ipctl/options"
-	"io/ioutil"
-	"strings"
+	"github.com/Streamer272/cool/check"
+	"github.com/zpatrick/go-config"
 )
 
 func Get(name string) string {
-	content, err := ioutil.ReadFile("/etc/ipctl/ipctl.json")
-	handle_error.HandleError(err)
+	etcFile := config.NewINIFile("/etc/ipctl/config")
+	content := config.NewConfig([]config.Provider{etcFile})
+	err := content.Load()
+	check.Check(err)
 
-	var opt options.Options
-	err = json.Unmarshal(content, &opt)
-	handle_error.HandleError(err)
+	value, err := content.String(name)
+	check.Check(err)
 
-	switch strings.ToLower(name) {
-	case "command":
-		return opt.Command
-	case "interval":
-		return opt.GetIntervalString()
-	case "current":
-		return opt.Current
-	default:
-		handle_error.HandleError(errors.New("name is not valid"))
-		return ""
-	}
+	return value
 }
 
-func GetAll() string {
-	content, err := ioutil.ReadFile("/etc/ipctl/ipctl.json")
-	handle_error.HandleError(err)
-	return string(content)
-}
+func GetConfigFiles() string {
+	etcFile := config.NewINIFile("/etc/ipctl/config")
+	content := config.NewConfig([]config.Provider{etcFile})
+	err := content.Load()
+	check.Check(err)
 
-func Set(name string, value string) {
-	content, err := ioutil.ReadFile("/etc/ipctl/ipctl.json")
-	handle_error.HandleError(err)
-
-	var opt options.Options
-	err = json.Unmarshal(content, &opt)
-	handle_error.HandleError(err)
-
-	switch strings.ToLower(name) {
-	case "command":
-		opt.Command = value
-		break
-	case "interval":
-		opt.SetIntervalString(value)
-		break
-	case "current":
-		opt.Current = value
-		break
-	default:
-		handle_error.HandleError(errors.New("name is not valid"))
-		return
+	output := ""
+	for provider := range content.Providers {
+		output += string(provider)
 	}
 
-	updated, err := json.Marshal(opt)
-	handle_error.HandleError(err)
-
-	err = ioutil.WriteFile("/etc/ipctl/ipctl.json", []byte(updated), constants.PERMS)
-	handle_error.HandleError(err)
+	return output
 }
--- go.mod
+++ go.mod
@@ -8,9 +8,18 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.0.0 // indirect
 	github.com/Streamer272/cool v0.0.0-20220204172029-f3102f6401e5 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-ini/ini v1.66.4 // indirect
 	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/urfave/cli v1.22.5 // indirect
+	github.com/zpatrick/go-config v0.0.0-20191118215128-80ba6b3e54f6 // indirect
 	golang.org/x/sys v0.0.0-20220110181412-a018aaa089fe // indirect
+	gopkg.in/yaml.v2 v2.2.5 // indirect
 )
